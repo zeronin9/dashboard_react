7c4f98f61cb02ffb9cf4320ce9d17312b7222012
zeronin9 <hidayat.mwh@gmail.com>
2025-10-08 10:45:17 +0800

    hasil menggunakan data default

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
-    open: true
+    port: 5173,  // atau port lain
+    open: true,
+    host: true
   }
 });
